945b9f7989c6b68956ecc85963a6378c2b9199a3
Choi <cpg627@gmail.com>
2016-08-05 17:30:30 +0900

    정리를 하였으나, 버튼이 동시에 눌러짐

--- app/src/main/java/com/ppro/p_choi/vkr_autotest/MainActivity.java
+++ app/src/main/java/com/ppro/p_choi/vkr_autotest/MainActivity.java
@@ -17,20 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         //버튼에 리스너 달기기
         btn_1_1=(Button)findViewById(R.id.btn_1_1);
-        btn_1_1.setOnClickListener(listener);
         btn_1_2=(Button)findViewById(R.id.btn_1_2);
+
+        Button.OnClickListener listener = new Button.OnClickListener(){
+            public void onClick(View v){
+                switch (v.getId()){
+                    case R.id.btn_1_1:
+                        System.out.println("1_1 눌림");
+                    case R.id.btn_1_2:
+                        System.out.println("1_2 눌림");
+                        break;
+                }
+            }
+        };//이부분에서 세미콜론;을 찍는것을잊어서는안된다
+        btn_1_1.setOnClickListener(listener);
         btn_1_2.setOnClickListener(listener);
+
     }
     //이벤트 핸들러
-    Button.OnClickListener listener = new Button.OnClickListener(){
-        public void onClick(View v){
-            switch (v.getId()){
-                case R.id.btn_1_1:
-                    System.out.println("1_1 눌림");
-                case R.id.btn_1_2:
-                    System.out.println("1_2 눌림");
-                    break;
-            }
-        }
-    };//이부분에서 세미콜론;을 찍는것을잊어서는안된다
+
 }
