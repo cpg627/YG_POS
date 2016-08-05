5cfabff9843c1f30276b6cf8a2261167d533bf51
Choi <cpg627@gmail.com>
2016-08-05 17:20:30 +0900

    Button event setting success!

--- app/src/main/java/com/ppro/p_choi/vkr_autotest/MainActivity.java
+++ app/src/main/java/com/ppro/p_choi/vkr_autotest/MainActivity.java
@@ -1,141 +1,36 @@
 package com.ppro.p_choi.vkr_autotest;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.util.ArrayList;
+import com.ppro.p_choi.vkr_autotest.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
+    Button btn_1_1, btn_1_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        //리스트 뷰 화면생성
-        ListView listView1 = (ListView) findViewById(R.id.listview1);
-        //리스트 뷰용 Data원본 준비
-        final ArrayList<String> arMenu = new ArrayList<String>();
-
-        Button.OnClickListener onClickListener = new Button.OnClickListener() {
-            public void onClick(View view) {
-
-                switch (view.getId()) {
-                    case R.id.btn_1_1:
-                        arMenu.add("아메리카노");
-                        break;
-                    case R.id.btn_1_2:
-                        arMenu.add("용궁라떼");
-                        break;
-                }
+        //버튼에 리스너 달기기
+        btn_1_1=(Button)findViewById(R.id.btn_1_1);
+        btn_1_1.setOnClickListener(listener);
+        btn_1_2=(Button)findViewById(R.id.btn_1_2);
+        btn_1_2.setOnClickListener(listener);
+    }
+    //이벤트 핸들러
+    Button.OnClickListener listener = new Button.OnClickListener(){
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.btn_1_1:
+                    System.out.println("1_1 눌림");
+                case R.id.btn_1_2:
+                    System.out.println("1_2 눌림");
+                    break;
             }
-        };
-
-        //어댑터 준비
-        ArrayAdapter<String> Adapter;
-        Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,arMenu);
-
-        //어댑터 연결
-        listView1.setAdapter(Adapter);
-        //항목을 선택하는 모드
-        listView1.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        //항목 사이의 구분선 지정
-        listView1.setDivider(new ColorDrawable(Color.WHITE));
-        //구분선의 높이 지정
-        listView1.setDividerHeight(2);
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.ppro.p_choi.vkr_autotest/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.ppro.p_choi.vkr_autotest/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
+    };//이부분에서 세미콜론;을 찍는것을잊어서는안된다
 }
